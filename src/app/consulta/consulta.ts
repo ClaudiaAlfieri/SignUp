37bf86370a330978900d39329509cace30c3a357
ClaudiaAlfieri <claudia.jorge86@gmail.com>
2026-09-21 22:48:40 +0100

    Update Consulta.ts

--- src/app/consulta/consulta.ts
+++ src/app/consulta/consulta.ts
@@ -1,9 +1,30 @@
 import { Component } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatCardModule } from '@angular/material/card';
+import { MatTableModule } from '@angular/material/table';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
+import { ClienteService } from '../cliente';
+
 
 @Component({
   selector: 'app-consulta',
-  imports: [],
+  imports: [
+    MatInputModule,
+    MatCardModule,
+    MatTableModule,
+    FormsModule,
+    MatIconModule,
+    MatButtonModule,
+    FlexLayoutModule
+  ],
   templateUrl: './consulta.html',
   styleUrl: './consulta.scss',
 })
-export class Consulta {}
+export class Consulta {
+  constructor(private clienteService: ClienteService) {
+    
+  }
+}
